de5d34426a5ff98e549ebfc2d80f8036b46ad7f4
Kaviyavarshini C S <kaviyavarshinics03@gmail.com>
2025-04-11 21:16:59 +0530

    Time: 7 ms (16.22%), Space: 44 MB (76.04%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String s1 = Integer.toString(x);
+        StringBuffer sb = new StringBuffer(s1);
+        String s2 = sb.reverse().toString();
+        if(s1.equals(s2)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
