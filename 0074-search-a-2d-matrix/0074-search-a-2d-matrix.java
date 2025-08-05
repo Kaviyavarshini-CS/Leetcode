8426e545d71c86a3026287ccf519242191edeec4
Kaviyavarshini C S <kaviyavarshinics03@gmail.com>
2025-08-05 17:18:36 +0530

    Time: 0 ms (100%), Space: 42 MB (81.41%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        boolean present = false;
+        for(int i=0;i<matrix.length;i++){
+            if(matrix[i][0]<=target && matrix[i][matrix[0].length-1]>=target){
+                for(int j=0;j<matrix[0].length;j++){
+                    if(matrix[i][j]==target) {
+                        present = true;
+                        break;
+                    }
+                }
+            }
+            if(present==true) return true;
+        }
+        return false;
+    }
+}
